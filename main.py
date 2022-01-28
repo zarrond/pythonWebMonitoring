64dac2597442295849abe366a5c5dfb2e44a5650
zarrond <47771154+zarrond@users.noreply.github.com>
2022-01-28 03:00:56 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import requests
+import os
+from datetime import datetime as d
+import time
+
+dir = os.path.dirname(__file__)
+
+while True:
+
+    with open(dir + "/data.txt", "a") as f:
+        s = requests.get('http://api.ipify.org/').text
+        f.write(s + " : " + d.now().strftime("%d/%m/%Y at %H:%M:%S") +"\n")
+    
+    time.sleep(10)
